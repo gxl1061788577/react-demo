227cb729ea148a2a6405e26ffd00105cf3f42e90
guoxiaolong <guoxiaolong@shall-buy.com>
2021-07-03 17:57:40 +0800

    test:sass--global-params

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/reset.css';
+import './assets/css/reset.scss';
 // import './assets/css/color.less';
 import App from './views/app/App';
 import reportWebVitals from './reportWebVitals';
